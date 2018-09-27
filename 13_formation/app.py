614511f8d58aa704e6550dbc1c21365b9dcafa5a
Adil Gondal <agondal@stuy.edu>
2018-09-27 16:46:06 -0400

    finished hw

---
+++ 13_formation/app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template , request
+app = Flask(__name__)
+
+
+@app.route("/")
+def func():
+    print(app)
+    return render_template("form.html")
+@app.route("/auth")
+def authenticate():
+    print(app)
+    print(request)
+    print(request.args)
+    #print(request.args['username'])
+    #print(request.method)
+    result = request.args['username']
+    method=request.method
+    return render_template("form2.html",name=result,methodUsed=method)
+
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
+
