b2ae63e8e251774908907e96ca34b227c88c4328
Adil Gondal <agondal@stuy.edu>
2018-10-04 19:08:34 -0400

    course db

--- 16_csv2db/db_builder.py
+++ 16_csv2db/db_builder.py
@@ -1,7 +1,7 @@
-#Clyde "Thluffy" Sinclair
-#SoftDev1 pd0
-#SQLITE3 BASICS
-#2018-10-04
+# Tank Engines-- Adil Gondal, Shufali Gupta
+# Softdev1 pd7
+# K16: No Trouble
+# 2018-10-04
 
 import sqlite3   #enable control of an sqlite database
 import csv       #facilitates CSV I/O
@@ -11,25 +11,34 @@ DB_FILE="discobandit.db"
 
 db = sqlite3.connect(DB_FILE) #open if file exists, otherwise create
 c = db.cursor()               #facilitate db ops
-c.execute("CREATE TABLE discobandit (name,age,id)")
+c.execute("CREATE TABLE discobandit (name TEXT,age INTGEGER,id INTEGER PRIMARY KEY)")
 
 def peep_open():
     with open("peeps.csv") as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
-            c.execute( "INSERT INTO discobandit (name,age,id) VALUES ('" + row["name"] +"','" + row["age"] + "','" + row["id"] + "')")
+            c.execute( "INSERT INTO discobandit VALUES ('" + row["name"] +"', '" +row["age"]+ "','" + row["id"] + "')")
 
 peep_open()
-      
-
-#==========================================================
-#INSERT YOUR POPULATE CODE IN THIS ZONE
-
 #c.execute("SELECT * FROM discobandit")
 #print(c.fetchall())
-
-#==========================================================
-
 db.commit() #save changes
 db.close()  #close database
+
+db = sqlite3.connect("courses.db")
+cur = db.cursor()
+cur.execute("CREATE TABLE courses (code TEXT, mark INTEGER,id INTEGER)")
+
+def course_open():
+    with open("courses.csv") as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            cur.execute( "INSERT INTO courses VALUES ('" + row["code"] +"', '" +row["mark"]+ "','" + row["id"] + "')")
+
+course_open()
+db.commit()
+db.close()
+
+
+
 
