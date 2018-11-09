0d823e9ec7e0d8a1ad934f9cab6dda13bae1fd49
Adil Gondal <agondal@stuy.edu>
2018-11-09 08:41:03 -0500

    carousel

--- 16_csv2db/db_builder.py
+++ 16_csv2db/db_builder.py
@@ -11,29 +11,28 @@ DB_FILE="discobandit.db"
 
 db = sqlite3.connect(DB_FILE) #open if file exists, otherwise create
 c = db.cursor()               #facilitate db ops
-c.execute("CREATE TABLE discobandit (name TEXT,age INTGEGER,id INTEGER PRIMARY KEY)") # creates discobandit table
+
+
+c.execute("CREATE TABLE peeps (name TEXT,age INTGEGER,id INTEGER PRIMARY KEY)") # creates peeps table
 
 def peep_open():
     with open("peeps.csv") as csvfile: 
         reader = csv.DictReader(csvfile)
         for row in reader: # goes row by row through csv file
-            c.execute( "INSERT INTO discobandit VALUES ('" + row["name"] +"', '" +row["age"]+ "','" + row["id"] + "')") #inserts new row into discobandit table
+            c.execute( "INSERT INTO peeps VALUES ('" + row["name"] +"', '" +row["age"]+ "','" + row["id"] + "')") #inserts new row into discobandit table
 
 peep_open()
 #c.execute("SELECT * FROM discobandit")
 #print(c.fetchall())
-db.commit() #save changes
-db.close()  #close database
 
-db = sqlite3.connect("courses.db")
-cur = db.cursor()
-cur.execute("CREATE TABLE courses (code TEXT, mark INTEGER,id INTEGER)") # creates table for courses
+
+c.execute("CREATE TABLE courses (code TEXT, mark INTEGER,id INTEGER)") # creates table for courses
 
 def course_open():
     with open("courses.csv") as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
-            cur.execute( "INSERT INTO courses VALUES ('" + row["code"] +"', '" +row["mark"]+ "','" + row["id"] + "')")
+            c.execute( "INSERT INTO courses VALUES ('" + row["code"] +"', '" +row["mark"]+ "','" + row["id"] + "')")
 
 course_open()
 db.commit()
