c57182c2a6c5edd0b956719efcc2863c7782c9c7
Adil Gondal <agondal@stuy.edu>
2018-11-13 13:24:20 -0500

    url

--- 24_rest/app.py
+++ 24_rest/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def root():
-    u = urllib.urlopen
+    u = urllib.urlopen("https://api.nasa.gov/planetary/apod?api_key=NNKOjkoul8n1CH18TWA9gwngW1s1SmjESPjNoUFo")
     return render_template("index.html",pic = data['url'])
 
 
